b8b253d2ce9a6ccaefe37c07c552764863e10435
Khanh Luong <khanhld1910@gmail.com>
2022-02-14 15:03:05 +0700

    implement lazy minting

--- my-wave-portal/contracts/Cohart.sol
+++ my-wave-portal/contracts/Cohart.sol
@@ -4,20 +4,29 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Cohart is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
-    Counters.Counter private _tokenIdCounter;
+contract Cohart is ERC721, ERC721URIStorage, Ownable, EIP712 {
+    
+    string private constant SIGNING_DOMAIN = "Cohart-LazyNFT-Voucher";
+    string private constant SIGNATURE_VERSION = "1";
 
-    constructor() ERC721("Cohart", "COHART") {}
+    mapping (uint256 => address) tokenCreators;
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    constructor()
+        ERC721("Cohart", "COHART")
+        EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION) {
+        
+        }
+
+    /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain. A signed voucher can be redeemed for a real NFT using the redeem function.
+    struct NFTVoucher {
+        uint256 tokenId;
+        uint256 minPrice;
+        string uri;
+        bytes signature;
     }
 
     // The following functions are overrides required by Solidity.
@@ -33,5 +42,64 @@ contract Cohart is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+
+    /// @notice Returns the chain id of the current blockchain.
+    /// @dev This is used to workaround an issue with ganache returning different values from the on-chain chainid() function and
+    ///  the eth_chainId RPC method. See https://github.com/protocol/nft-website/issues/121 for context.
+    function getChainID() external view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+
+    /// @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
+    /// @param voucher An NFTVoucher to hash.
+    function _hash(NFTVoucher calldata voucher) internal view returns (bytes32) {
+        return _hashTypedDataV4(keccak256(abi.encode(
+        keccak256("NFTVoucher(uint256 tokenId,uint256 minPrice,string uri)"),
+        voucher.tokenId,
+        voucher.minPrice,
+        keccak256(bytes(voucher.uri))
+        )));
+    }
+
+
+    /// @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
+    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
+    /// @param voucher An NFTVoucher describing an unminted NFT.
+    function _verify(NFTVoucher calldata voucher) internal view returns (address) {
+        bytes32 digest = _hash(voucher);
+        return ECDSA.recover(digest, voucher.signature);
+    }
+
+    /// @notice Redeems an NFTVoucher for an actual NFT, creating it in the process.
+    /// @param redeemer The address of the account which will receive the NFT upon success.
+    /// @param voucher A signed NFTVoucher that describes the NFT to be redeemed.
+    function redeem(address redeemer, NFTVoucher calldata voucher) public payable returns (uint256) {
+        // make sure signature is valid and get the address of the signer
+        address signer = _verify(voucher);
+
+        // make sure that the redeemer is paying enough to cover the buyer's cost
+        require(msg.value >= voucher.minPrice, "Insufficient funds to redeem");
+
+        // uint256 tokenId = _tokenIdCounter.current();
+        // _tokenIdCounter.increment();
+        // first assign the token to the signer, to establish provenance on-chain
+        _mint(signer, voucher.tokenId);
+        // TODO: do we need this line?
+        // tokenCreators[tokenId] = msg.sender;
+
+        _setTokenURI(voucher.tokenId, voucher.uri);
+        
+        // transfer the token to the redeemer
+        _transfer(signer, redeemer, voucher.tokenId);
+
+        // TODO: share profit ??
+
+        return voucher.tokenId;
     }
 }
--- my-wave-portal/scripts/run-cohart.js
+++ my-wave-portal/scripts/run-cohart.js
@@ -1,20 +1,44 @@
+const { ethers, waffle } = require('hardhat')
+const provider = waffle.provider
+
+const CONTRACT_NAME = 'Cohart'
+
+const { LazyMinter } = require('../lib/LazyMinter')
+
 const main = async () => {
-  const myContractFactory = await hre.ethers.getContractFactory("Cohart");
-  const CohartContract = await myContractFactory.deploy({
-    // value: hre.ethers.utils.parseEther("0.1")
-  });
-  await CohartContract.deployed();
-  console.log("Contract address:", CohartContract.address);
+  const myContractFactory = await ethers.getContractFactory(CONTRACT_NAME)
+  const contract = await myContractFactory.deploy()
+
+  await contract.deployed()
+  console.log('Contract address:', contract.address)
+
+  
 
   // get contract balance
 
-  let contractBalance = await hre.ethers.provider.getBalance(
-    CohartContract.address
+  let contractBalance = await provider.getBalance(contract.address)
+  console.log('Contract balance:', ethers.utils.formatEther(contractBalance))
+
+  const [owner, personA, personB, personC, personD, ...others] = await ethers.getSigners()
+
+  const lazyMinter = new LazyMinter({ contract, signer: personA })
+
+  const nftVoucher = await lazyMinter.createVoucher(
+    0,
+    'hihi_uri_000',
+    ethers.utils.parseEther('0.1')
   )
-  console.log(
-    'Contract balance:',
-    hre.ethers.utils.formatEther(contractBalance)
-  )
+
+  // TODO: save this vocher to backend
+
+  // TODO: redem the vocer
+  const tokenId = await contract.redeem(personB.address, nftVoucher, {
+    value: ethers.utils.parseEther('0.1')
+  })
+  console.log({ tokenId })
+
+
+  
 
   /*
    * Let's try two waves now
@@ -25,24 +49,24 @@ const main = async () => {
   // const waveTxn2 = await CohartContract.wave("This is wave #2");
   // await waveTxn2.wait();
 
-  // contractBalance = await hre.ethers.provider.getBalance(CohartContract.address);
+  // contractBalance = await ethers.provider.getBalance(CohartContract.address);
   // console.log(
   //   "Contract balance:",
-  //   hre.ethers.utils.formatEther(contractBalance)
+  //   ethers.utils.formatEther(contractBalance)
   // );
 
   // let allWaves = await CohartContract.getAllWaves();
   // console.log(allWaves);
-};
+}
 
 const runMain = async () => {
   try {
-    await main();
-    process.exit(0);
+    await main()
+    process.exit(0)
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.log(error)
+    process.exit(1)
   }
-};
+}
 
-runMain();
+runMain()
